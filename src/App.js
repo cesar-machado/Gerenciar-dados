ff5e3828d2e19235b357a9f995be58065ed4cce5
cesar-machado <cesarmach19@gmail.com>
2020-05-04 19:35:18 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './app.css'
+
+import Header from './components/Header/Header'
+import Menu from './components/menu/Menu'
+import Main from './components/main/Main'
+import Footer from './components/Footer/Footer'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header  />
+      <Menu />
+      <Main />
+      <Footer />
     </div>
   );
 }
